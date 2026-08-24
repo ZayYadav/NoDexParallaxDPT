f3a011754dd9897ad3b8f1806b2bc244dd4f6930
JITENDRA YADAV <106445997+ZayYadav@users.noreply.github.com>
2026-08-25 01:07:18 +0530

    hard: use renamed component factory in protected manifests

--- parallax/src/main/java/com/parallax/parallax/res/ApkManifestEditor.java
+++ parallax/src/main/java/com/parallax/parallax/res/ApkManifestEditor.java
@@ -12,73 +12,42 @@ import pxb.android.axml.AxmlParser;
  * @author parallax
  */
 public class ApkManifestEditor {
-    /**
-     * Write app name to xml and ensure the protection warning can fetch its HTTPS video.
-     */
     public static void writeApplicationName(String inManifestFile, String outManifestFile, String newApplicationName){
         ModificationProperty property = new ModificationProperty();
         property.addApplicationAttribute(new AttributeItem(NodeValue.Application.NAME,newApplicationName));
         property.addUsesPermission("android.permission.INTERNET");
-
         FileProcesser.processManifestFile(inManifestFile, outManifestFile, property);
-
     }
 
-    /**
-     * Write appComponentFactory to xml. The APK bootstrap rewrites the Application first,
-     * so derive the matching randomized-shell protection factory from that name.
-     */
     public static void writeAppComponentFactory(String inManifestFile, String outManifestFile, String newComponentFactory){
         String applicationName = getApplicationName(inManifestFile);
         if (applicationName != null && applicationName.endsWith(".ParallaxKoChummiDedo")) {
             int lastDot = applicationName.lastIndexOf('.');
             if (lastDot > 0) {
-                newComponentFactory = applicationName.substring(0, lastDot + 1)
-                        + "ParallaxProtectionFactory";
+                newComponentFactory = applicationName.substring(0, lastDot + 1) + "ParallaxTGUser";
             }
         }
-
         ModificationProperty property = new ModificationProperty();
         property.addApplicationAttribute(new AttributeItem("appComponentFactory",newComponentFactory));
-
         FileProcesser.processManifestFile(inManifestFile, outManifestFile, property);
-
     }
 
-    /**
-     * Write debuggable field
-     */
     public static void writeDebuggable(String inManifestFile, String outManifestFile, String debuggable){
         ModificationProperty property = new ModificationProperty();
         property.addApplicationAttribute(new AttributeItem("debuggable",debuggable));
-
         FileProcesser.processManifestFile(inManifestFile, outManifestFile, property);
-
     }
 
-    /**
-     * Get AndroidManifest.xml attr value
-     * @param file AndroidManifest.xml file
-     * @param tag tag name
-     * @param ns namespace
-     * @param attrName attr name
-     * @return
-     */
     public static String getAttributeValue(String file, String tag, String ns, String attrName){
         byte[] axmlData = IoUtils.readFile(file);
         AxmlParser axmlParser = new AxmlParser(axmlData);
         try {
             while (axmlParser.next() != AxmlParser.END_FILE) {
-                if (axmlParser.getAttrCount() != 0) {
-                     if(!axmlParser.getName().equals(tag)) {
-                         continue;
-                     }
+                if (axmlParser.getAttrCount() != 0 && !axmlParser.getName().equals(tag)) {
+                    continue;
                 }
-
                 for (int i = 0; i < axmlParser.getAttrCount(); i++) {
-
                     if (ns == null || axmlParser.getNamespacePrefix().equals(ns)) {
-
                         if(axmlParser.getAttrName(i).equals(attrName)) {
                             return (String) axmlParser.getAttrValue(i);
                         }
@@ -91,44 +60,21 @@ public class ApkManifestEditor {
         return null;
     }
 
-    /**
-     * Get android:name value from AndroidManifest.xml
-     */
     public static String getApplicationName(String file) {
         String attributeValue = getAttributeValue(file, "application", "android", "name");
-
         attributeValue = attributeValue == null ? getAttributeValue(file, "application", "dist", "name") : attributeValue;
-
-        /*
-         * xmlns:android="http://schemas.android.com/apk/res/android" xmlns:n1="http://schemas.android.com/apk/distribution"
-         * The result parsed by ManifestEditor seems to have an issue, as it recognizes Android as n1 when there are
-         * two namespaces in the manifest file at the same time
-         * */
-
-        /*
-         * xmlns:android="http://schemas.android.com/apk/res/android" xmlns:n1="http://schemas.android.com/apk/distribution"
-         * ManifestEditor 解析的结果似乎是存在问题的，当清单文件中同时存在两个命名空间时它会把 android 识别为 n1
-         * */
         attributeValue = attributeValue == null ? getAttributeValue(file, "application", null,"name") : attributeValue;
-
         return attributeValue;
     }
 
-    /**
-     * Get android:appComponentFactory value from AndroidManifest.xml
-     */
     public static String getAppComponentFactory(String file) {
         String attributeValue = getAttributeValue(file, "application", "android", "appComponentFactory");
-
         attributeValue = attributeValue == null ? getAttributeValue(file, "application", "android", "appComponentFactory") : attributeValue;
-
         attributeValue = attributeValue == null ? getAttributeValue(file, "application", null,"appComponentFactory") : attributeValue;
-
         return attributeValue;
     }
 
     public static String getPackageName(String file) {
         return getAttributeValue(file,"manifest","android","package");
     }
-
 }
