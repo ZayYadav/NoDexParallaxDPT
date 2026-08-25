9aa6c1f6af03493fb3e1d3781ead10910fc471c7
JITENDRA YADAV <106445997+ZayYadav@users.noreply.github.com>
2026-08-25 22:43:30 +0530

    Remove old blocked receiver name

--- shell/src/main/java/com/parallax/shell/ParallaxSnapUser.java
+++
@@ -1,9 +0,0 @@
-package com.parallax.shell;
-
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-
-public final class ParallaxSnapUser extends BroadcastReceiver {
-    @Override public void onReceive(Context context, Intent intent) { }
-}
