39e84ebfd8146510f4dc3fe16ff9dc9c63b6bc32
JITENDRA YADAV <106445997+ZayYadav@users.noreply.github.com>
2026-08-30 01:54:32 +0530

    fix: rename VM JNI exports for Parallax.Enc

--- shell/src/main/cpp/parallax_vm_exports.cpp
+++ shell/src/main/cpp/parallax_vm_exports.cpp
@@ -2,43 +2,43 @@
 
 extern "C" {
 
-JNIEXPORT jint JNICALL Java_parallax_vm_Parallax16_hvi0(JNIEnv *env, jclass cls, jint methodId) {
+JNIEXPORT jint JNICALL Java_Parallax_Enc_CrackWarTeamMC_hvi0(JNIEnv *env, jclass cls, jint methodId) {
     return highValueVmI0(env, cls, methodId);
 }
 
-JNIEXPORT jint JNICALL Java_parallax_vm_Parallax16_hvi1(JNIEnv *env, jclass cls, jint methodId, jint a0) {
+JNIEXPORT jint JNICALL Java_Parallax_Enc_CrackWarTeamMC_hvi1(JNIEnv *env, jclass cls, jint methodId, jint a0) {
     return highValueVmI1(env, cls, methodId, a0);
 }
 
-JNIEXPORT jint JNICALL Java_parallax_vm_Parallax16_hvi2(JNIEnv *env, jclass cls, jint methodId, jint a0, jint a1) {
+JNIEXPORT jint JNICALL Java_Parallax_Enc_CrackWarTeamMC_hvi2(JNIEnv *env, jclass cls, jint methodId, jint a0, jint a1) {
     return highValueVmI2(env, cls, methodId, a0, a1);
 }
 
-JNIEXPORT jint JNICALL Java_parallax_vm_Parallax16_hvi3(JNIEnv *env, jclass cls, jint methodId, jint a0, jint a1, jint a2) {
+JNIEXPORT jint JNICALL Java_Parallax_Enc_CrackWarTeamMC_hvi3(JNIEnv *env, jclass cls, jint methodId, jint a0, jint a1, jint a2) {
     return highValueVmI3(env, cls, methodId, a0, a1, a2);
 }
 
-JNIEXPORT jint JNICALL Java_parallax_vm_Parallax16_hvi4(JNIEnv *env, jclass cls, jint methodId, jint a0, jint a1, jint a2, jint a3) {
+JNIEXPORT jint JNICALL Java_Parallax_Enc_CrackWarTeamMC_hvi4(JNIEnv *env, jclass cls, jint methodId, jint a0, jint a1, jint a2, jint a3) {
     return highValueVmI4(env, cls, methodId, a0, a1, a2, a3);
 }
 
-JNIEXPORT void JNICALL Java_parallax_vm_Parallax16_hvv0(JNIEnv *env, jclass cls, jint methodId) {
+JNIEXPORT void JNICALL Java_Parallax_Enc_CrackWarTeamMC_hvv0(JNIEnv *env, jclass cls, jint methodId) {
     highValueVmV0(env, cls, methodId);
 }
 
-JNIEXPORT void JNICALL Java_parallax_vm_Parallax16_hvv1(JNIEnv *env, jclass cls, jint methodId, jint a0) {
+JNIEXPORT void JNICALL Java_Parallax_Enc_CrackWarTeamMC_hvv1(JNIEnv *env, jclass cls, jint methodId, jint a0) {
     highValueVmV1(env, cls, methodId, a0);
 }
 
-JNIEXPORT void JNICALL Java_parallax_vm_Parallax16_hvv2(JNIEnv *env, jclass cls, jint methodId, jint a0, jint a1) {
+JNIEXPORT void JNICALL Java_Parallax_Enc_CrackWarTeamMC_hvv2(JNIEnv *env, jclass cls, jint methodId, jint a0, jint a1) {
     highValueVmV2(env, cls, methodId, a0, a1);
 }
 
-JNIEXPORT void JNICALL Java_parallax_vm_Parallax16_hvv3(JNIEnv *env, jclass cls, jint methodId, jint a0, jint a1, jint a2) {
+JNIEXPORT void JNICALL Java_Parallax_Enc_CrackWarTeamMC_hvv3(JNIEnv *env, jclass cls, jint methodId, jint a0, jint a1, jint a2) {
     highValueVmV3(env, cls, methodId, a0, a1, a2);
 }
 
-JNIEXPORT void JNICALL Java_parallax_vm_Parallax16_hvv4(JNIEnv *env, jclass cls, jint methodId, jint a0, jint a1, jint a2, jint a3) {
+JNIEXPORT void JNICALL Java_Parallax_Enc_CrackWarTeamMC_hvv4(JNIEnv *env, jclass cls, jint methodId, jint a0, jint a1, jint a2, jint a3) {
     highValueVmV4(env, cls, methodId, a0, a1, a2, a3);
 }
 
